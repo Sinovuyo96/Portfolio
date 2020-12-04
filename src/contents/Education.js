a639cbcc1b8e163ba7426606480aaf6dcadff13b
unknown <sinovuyosolani96@gmail.com>
2020-12-04 12:53:59 -0800

    committed

--- src/contents/Education.js
+++ src/contents/Education.js
@@ -7,8 +7,9 @@ render() {
 return (
 <div className="condiv">
 <h1 className="subtopic">My Education</h1>
-<Widecard title="NDiploma Information Technology" where="WSU University" from="February 2016" to="November 2018"/>
 <Widecard title="Pakamani | SSS" where="Pakamani Senior Secondary School" from="2013" to="2015"/>
+<Widecard title="NDiploma Information Technology" where="WSU University" from="February 2016" to="November 2018"/>
+
 
 <h1 className="subtopic">Certification</h1>
 
